a553d84fc3b7a227826bb87cc1aac0f772a963fa
bhsenna <bernardo.h24@aluno.ifsc.edu>
2025-03-26 22:15:50 -0300

    Serviços

---
+++ DatabaseAutoPecas/ServicoParaProduto.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ServicoParaProduto]
+(
+	[ServicoID] INT NOT NULL, 
+    [ProdutoID] INT NOT NULL, 
+    CONSTRAINT [FK_ServicoParaProduto_Servicos] FOREIGN KEY ([ServicoID]) REFERENCES [Servicos]([ServicoID]), 
+    CONSTRAINT [FK_ServicoParaProduto_Produtos] FOREIGN KEY ([ProdutoID]) REFERENCES [Produtos]([ProdutoID]) 
+)
---
+++ DatabaseAutoPecas/Servicos.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Servicos]
+(
+	[ServicoID] INT NOT NULL PRIMARY KEY, 
+    [MargemLucro] TINYINT NOT NULL DEFAULT 0
+)
