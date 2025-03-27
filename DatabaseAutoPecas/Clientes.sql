fa9d0afa9e86250a345a3daab273c7a1775b4416
bhsenna <bernardo.h24@aluno.ifsc.edu>
2025-03-26 22:08:20 -0300

    Cliente + Veículos

---
+++ DatabaseAutoPecas/Clientes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Clientes]
+(
+	[ClienteID] INT NOT NULL PRIMARY KEY, 
+    [NomeCliente] NVARCHAR(250) NOT NULL, 
+    [Telefone] CHAR(11) NULL, 
+    [Email] NVARCHAR(150) NULL
+)
---
+++ DatabaseAutoPecas/Veiculos.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Veiculos]
+(
+	[VeiculoID] INT NOT NULL PRIMARY KEY, 
+    [PlacaVeiculo] NCHAR(8) NULL, 
+    [Modelo] NVARCHAR(250) NULL, 
+    [Marca] NVARCHAR(100) NULL, 
+    [ClienteID] INT NOT NULL, 
+    CONSTRAINT [FK_Veiculos_Clientes] FOREIGN KEY ([ClienteID]) REFERENCES [Clientes]([ClienteID])
+)
