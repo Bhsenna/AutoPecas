054714e1ee1064794d893b4c8b90f52d144fd066
bhsenna <bernardo.h24@aluno.ifsc.edu>
2025-03-26 21:32:47 -0300

    Add project files.

---
+++ DatabaseAutoPecas/Produtos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Produtos]
+(
+	[ProdutoID] INT NOT NULL PRIMARY KEY, 
+    [NomeProduto] NVARCHAR(MAX) NOT NULL, 
+    [CodigoProduto] INT NOT NULL, 
+    [PrecoCusto] MONEY NULL, 
+    [Status] BIT NOT NULL DEFAULT 0
+)
