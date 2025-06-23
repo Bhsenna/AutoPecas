b7d8b210e13e19a8a161afc8edbcea7102b14fb1
Júlio César Deretti <sulistacomfrio@gmail.com>
2025-06-23 00:08:44 -0300

    Tela de cadastro forçado para 1º login

---
+++ Salomao/Cadastros/CadUsuarioForcado.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Salomao.Cadastros
+{
+    partial class CadUsuarioForcado
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtNovoUsuario;
+        private System.Windows.Forms.TextBox txtNovaSenha;
+        private System.Windows.Forms.TextBox txtConfirmaSenha;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtNovoUsuario = new System.Windows.Forms.TextBox();
+            this.txtNovaSenha = new System.Windows.Forms.TextBox();
+            this.txtConfirmaSenha = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+
+            this.SuspendLayout();
+
+            // Labels
+            this.label1.Text = "Novo usuário:";
+            this.label1.Location = new System.Drawing.Point(20, 20);
+            this.label1.AutoSize = true;
+
+            this.label2.Text = "Nova senha:";
+            this.label2.Location = new System.Drawing.Point(20, 60);
+            this.label2.AutoSize = true;
+
+            this.label3.Text = "Confirme a senha:";
+            this.label3.Location = new System.Drawing.Point(20, 100);
+            this.label3.AutoSize = true;
+
+            // Textboxes
+            this.txtNovoUsuario.Location = new System.Drawing.Point(150, 17);
+            this.txtNovoUsuario.Width = 200;
+
+            this.txtNovaSenha.Location = new System.Drawing.Point(150, 57);
+            this.txtNovaSenha.Width = 200;
+            this.txtNovaSenha.UseSystemPasswordChar = true;
+
+            this.txtConfirmaSenha.Location = new System.Drawing.Point(150, 97);
+            this.txtConfirmaSenha.Width = 200;
+            this.txtConfirmaSenha.UseSystemPasswordChar = true;
+
+            // Button
+            this.btnCadastrar.Location = new System.Drawing.Point(150, 140);
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+
+            // FormCadastroUsuarioForcado
+            this.ClientSize = new System.Drawing.Size(380, 190);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNovoUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNovaSenha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtConfirmaSenha);
+            this.Controls.Add(this.btnCadastrar);
+            //this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            //this.MaximizeBox = false;
+            //this.MinimizeBox = false;
+            //this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro obrigatório de usuário";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
--- Salomao/Security/LoginService.cs
+++ Salomao/Security/LoginService.cs
@@ -9,44 +9,81 @@ namespace Salomao.Security
 {
     public class LoginService
     {
-        public static bool TentarLogin(string usuario, string senha, out string mensagemErro)
+        public static bool ExisteUsuario(string login)
         {
-            mensagemErro = string.Empty;
-            try
+            using var con = BancoSQLite.GetConnection();
+            string query = "SELECT COUNT(*) FROM Usuarios WHERE Login = @login";
+            using var cmd = new SQLiteCommand(query, con);
+            cmd.Parameters.AddWithValue("@login", login);
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            return count > 0;
+        }
+
+        public static void CriarUsuario(string login, string senha)
+        {
+            using var con = BancoSQLite.GetConnection();
+
+            string hash = PasswordManager.HashPassword(senha, out string salt);
+
+            string query = @"INSERT INTO Usuarios (Login, SenhaHash, Salt)
+                         VALUES (@login, @senha, @salt)";
+            using var cmd = new SQLiteCommand(query, con);
+            cmd.Parameters.AddWithValue("@login", login);
+            cmd.Parameters.AddWithValue("@senha", hash);
+            cmd.Parameters.AddWithValue("@salt", salt);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void TrocarSenha(string login, string novaSenha)
+        {
+            using var con = BancoSQLite.GetConnection();
+
+            string hash = PasswordManager.HashPassword(novaSenha, out string salt);
+
+            string query = @"UPDATE Usuarios SET SenhaHash = @senha, Salt = @salt WHERE Login = @login";
+            using var cmd = new SQLiteCommand(query, con);
+            cmd.Parameters.AddWithValue("@senha", hash);
+            cmd.Parameters.AddWithValue("@salt", salt);
+            cmd.Parameters.AddWithValue("@login", login);
+
+            cmd.ExecuteNonQuery();
+        }
+        public static bool TentarLogin(string usuario, string senha, out string mensagemErro, out bool isFirstLogin)
+        {
+            isFirstLogin = false;
+            mensagemErro = "";
+
+            using var con = BancoSQLite.GetConnection();
+            string query = "SELECT SenhaHash, Salt FROM Usuarios WHERE Login = @login";
+            using var cmd = new SQLiteCommand(query, con);
+            cmd.Parameters.AddWithValue("@login", usuario);
+
+            using var reader = cmd.ExecuteReader();
+
+            if (!reader.Read())
             {
-                using (SQLiteConnection con = BancoSQLite.GetConnection())
-                {
-                    using (SQLiteCommand cmd = new SQLiteCommand("SELECT SenhaHash, Salt FROM Usuarios WHERE Login = @usuario", con))
-                    {
-                        cmd.Parameters.AddWithValue("@usuario", usuario);
-
-                        using (var reader = cmd.ExecuteReader())
-                        {
-                            if (!reader.Read())
-                            {
-                                mensagemErro = "Usuário não encontrado.";
-                                return false;
-                            }
-
-                            var hash = reader["SenhaHash"].ToString();
-                            var salt = reader["Salt"].ToString();
-
-                            if (!PasswordManager.VerifyPassword(senha, hash, salt))
-                            {
-                                mensagemErro = "Senha incorreta.";
-                                return false;
-                            }
-                        }
-                    }
-                }
-
-                return true;
-            }
-            catch (Exception)
-            {
-                mensagemErro = "Erro interno na autenticação.";
+                mensagemErro = "Usuário não encontrado.";
                 return false;
             }
+
+            string senhaHash = reader.GetString(0);
+            string salt = reader.GetString(1);
+
+            bool senhaValida = PasswordManager.VerifyPassword(senha, senhaHash, salt);
+
+            if (!senhaValida)
+            {
+                mensagemErro = "Senha incorreta.";
+                return false;
+            }
+
+            if (usuario.ToLower() == "admin" && senha == "Auto@Admin")
+            {
+                isFirstLogin = true;
+            }
+
+            return true;
         }
     }
 }
